79668da65d7a46ae7266087cc1f77164cc298e25
AlexandraBlinova <testieduab@gmail.com>
2025-02-20 16:56:31 +0100

    Test project

---
+++ TestProject/src/main/java/TestProject.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class TestProject {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
